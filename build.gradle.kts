dca088fabe632e1360f79456557ac18403e98856
daoge_cmd <3523206925@qq.com>
2024-07-24 16:30:07 +0800

    fix: switch to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,54 @@ java {
     withSourcesJar()
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_17
-java.targetCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name.set("item-state-updater")
+                packaging = "jar"
+                description.set("Updates Minecraft: Bedrock Edition item states to the latest revision")
+                url.set("https://github.com/AllayMC/NBT")
+
+                scm {
+                    connection.set("scm:git:git://github.com/AllayMC/ItemStateUpdater.git")
+                    developerConnection.set("scm:git:ssh:/github.com/AllayMC/ItemStateUpdater.git")
+                    url.set("https://github.com/AllayMC/NBT")
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("CloudburstMC Team")
+                        organization.set("CloudburstMC")
+                        organizationUrl.set("https://github.com/CloudburstMC")
+                    }
+                    developer {
+                        name.set("AllayMC Team")
+                        organization.set("AllayMC")
+                        organizationUrl.set("https://github.com/AllayMC")
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    test {
+        useJUnitPlatform()
+    }
 }
